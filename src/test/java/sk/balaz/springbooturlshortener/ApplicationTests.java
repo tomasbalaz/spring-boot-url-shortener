b9c5145bda2d774b1f8cc13953d669d4d867b7f4
Tomas Balaz <balaztomas0@gmail.com>
2025-07-23 15:13:25 +0200

    Created spring-boot-url-shortener application

---
+++ src/test/java/sk/balaz/springbooturlshortener/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
