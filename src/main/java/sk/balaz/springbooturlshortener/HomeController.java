eee7ad7fe59449b7df9ebb27d8429a64914e05eb
Tomas Balaz <balaztomas0@gmail.com>
2025-07-25 13:31:35 +0200

    Updated project with thymeleaf starter

--- src/main/java/sk/balaz/springbooturlshortener/HomeController.java
+++ src/main/java/sk/balaz/springbooturlshortener/HomeController.java
@@ -1,18 +1,20 @@
 package sk.balaz.springbooturlshortener;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
   @GetMapping("/")
-  public String index() {
-    return "index.html";
+  public String index(Model model) {
+    model.addAttribute("title", "URL Shortener - Thymeleaf");
+    return "index";
   }
 
   @GetMapping("/about")
   public String about() {
-    return "about.html";
+    return "about";
   }
 }
