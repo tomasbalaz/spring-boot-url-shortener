8b6721dc016b712f664de21ebdb91d4b26b947c3
Tomas Balaz <balaztomas0@gmail.com>
2025-08-12 15:24:18 +0200

    Explained LazyLoadingException and @EntityGraph annotation

--- src/main/java/sk/balaz/springbooturlshortener/domain/repositories/ShortUrlRepository.java
+++ src/main/java/sk/balaz/springbooturlshortener/domain/repositories/ShortUrlRepository.java
@@ -1,5 +1,6 @@
 package sk.balaz.springbooturlshortener.domain.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import sk.balaz.springbooturlshortener.domain.entities.ShortUrl;
@@ -10,6 +11,8 @@ public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
 
   List<ShortUrl> findByIsPrivateFalseOrderByCreatedAtDesc();
 
+  //@Query("select su from ShortUrl su left join fetch su.createdBy where su.isPrivate = false order by su.createdAt desc")
   @Query("select su from ShortUrl su where su.isPrivate = false order by su.createdAt desc")
+  @EntityGraph(attributePaths = {"createdBy"})
   List<ShortUrl> findPublicShortUrls();
 }
