431e07095a5863177871d7fca2a68d1bc45306e8
Tomas Balaz <balaztomas0@gmail.com>
2025-08-28 15:13:49 +0200

    Created WebSecurityConfig

---
+++ src/main/java/sk/balaz/springbooturlshortener/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package sk.balaz.springbooturlshortener.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(CsrfConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(
+                    "/error", "/webjars/**", "/css/**", "/js/**", "/images/**",
+                    "/", "/short-urls", "/s/**", "/register", "/login"
+                ).permitAll()
+                .requestMatchers("/my-urls").authenticated()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+            );
+
+        return http.build();
+    }
+}
